65a73f16ad063c828ccfcbfda25fc97cd2ead726
Hadjis <hadjigregoriadesc@gmail.com>
2024-08-20 21:16:22 +0300

    Replaced packing with grid structure. Labels and some buttons added.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tkinter
-from tkinter import ttk
-from tkinter import messagebox
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import *
+from tkinter import ttk, messagebox, filedialog
 import os
 from PIL import Image
 import pillow_heif
@@ -45,32 +46,45 @@ def convert_all_heic_in_directory(input_folder, output_folder, max_workers=4):
             executor.submit(convert_heic_to_png, heic_file_path, output_folder)
 
 def start_conversion():
-    worker_input = 0
-    # Define input and output folders
-    #input_folder = input("Where are the original photos? Eg: 'C:/Users/Hadjis/Desktop/testfiles': ")  # Replace with your input folder
-    #output_folder = input("Where do you want to save the new photos? Eg: 'C:/Users/Hadjis/Desktop/outputfiles': ")  # Replace with your output folder
+    worker_input = 8
 
+    # Define input and output folders
     input_folder = "C:/Users/Hadjis/Desktop/testfiles" # Replace with your input folder
     output_folder = "C:/Users/Hadjis/Desktop/outputfiles" # Replace with your output folder
-
-    #worker_input = int(input("How many workers? Must be 1-8: "))
-    #while (worker_input > 8) or (worker_input < 1):
-    #    print("")
-    #    print("Incorrect input attributed to user error, please try again.")
-    #    worker_input = int(input("How many workers? Must be 1-8: "))
 
     start_time = time.time()
 
     # Convert all HEIC files in the directory
-    convert_all_heic_in_directory(input_folder, output_folder, max_workers=8)  # Adjust max_workers based on your CPU cores
+    convert_all_heic_in_directory(input_folder, output_folder, max_workers=worker_input)  # Adjust max_workers based on your CPU cores
     
     end_time = time.time()
 
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.2f} seconds")
 
-m = tkinter.Tk()
+def browse_input():
+    selection = filedialog.askdirectory()
+    folder_path.set(selection)
+    print(folder_path)
 
-ttk.Button(text= "Open", command= start_conversion).pack()
+root = tk.Tk()
 
-m.mainloop()
+label_font = tkFont.Font(family="Arial", size=12, weight=tkFont.BOLD)
+convert_font = tkFont.Font(family="Arial", size=12, weight=tkFont.BOLD)
+
+root.title('PicRelief')
+root.minsize(600, 600)
+
+folder_header = Label(root, text="Select Folders", bd=1, relief=SUNKEN, anchor=N, font=label_font)
+folder_header.grid(row=0)
+
+folder_path = StringVar()
+lbl1 = Label(root, textvariable=folder_path, width=40, bd=1, relief=SUNKEN, anchor=N)
+lbl1.grid(row=1, column=1)
+button2 = Button(text="Browse", command=browse_input)
+button2.grid(row=1, column=3)
+
+convert_button = Button(text= "Begin Conversion", font=convert_font, command= start_conversion)
+convert_button.grid(row=2)
+
+root.mainloop()
