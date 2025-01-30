284156477d73caa2cacad8927d45d93cd7c78d41
Hadjis2001 <hadjigregoriadesk@cardiff.ac.uk>
2025-01-30 21:56:27 +0000

    Changed theme and added settings section in UI.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter.font as tkFont
 from tkinter import *
 from tkinter import ttk, messagebox, filedialog
 from concurrent.futures import ThreadPoolExecutor
+import sv_ttk
 
 root = tk.Tk()
 
@@ -15,35 +16,40 @@ root.title('PicRelief')
 root.minsize(900, 600)
 root.maxsize(900, 600)
 
-separator_label = Label(root, text="", bd=1, relief=SUNKEN, anchor=CENTER)
+separator_label = ttk.Label(root, text="", borderwidth=1, relief=SUNKEN, anchor=CENTER)
 
-folder_header = Label(root, text="Select Folders", bd=1, relief=SUNKEN, anchor=CENTER, font=label_font, justify="center")
+folder_header = ttk.Label(root, text="Select Folders", borderwidth=1, relief=SUNKEN, anchor=CENTER, justify="center")
 folder_header.grid(row=0, column=0, columnspan=4, pady=10, sticky="nsew")
 
-lbl1 = Label(root, textvariable=input_selection, width=75, bd=1, relief=SUNKEN, anchor=W, justify="center")
+lbl1 = ttk.Label(root, textvariable=input_selection, width=65, borderwidth=1, relief=SUNKEN, anchor=W, justify="center")
 lbl1.grid(row=1, column=0, columnspan=4, pady=10)
 
-input_label = Label(root, text="Input Folder:", width=11, bd=1, anchor=E, justify="center")
+input_label = ttk.Label(root, text="Input Folder:", width=11, borderwidth=1, anchor=E, justify="center")
 input_label.grid(row=1, column=0, columnspan=1)
 
-button2 = Button(text="Browse...", command=browse_input)
+button2 = ttk.Button(text="Browse...", command=browse_input)
 button2.grid(row=1, column=3, columnspan=1)
 
-lbl2 = Label(root, textvariable=output_selection, width=75, bd=1, relief=SUNKEN, anchor=W, justify="center")
+lbl2 = ttk.Label(root, textvariable=output_selection, widt=65, borderwidth=1, relief=SUNKEN, anchor=W, justify="center")
 lbl2.grid(row=3, column=0, columnspan=4, pady=10)
 
-output_label = Label(root, text="Output Folder:", width=11, bd=1, anchor=E, justify="center")
+output_label = ttk.Label(root, text="Output Folder:", width=11, borderwidth=1, anchor=E, justify="center")
 output_label.grid(row=3, column=0, columnspan=1, pady=10)
 
-button3 = Button(text="Browse...", command=browse_output)
+button3 = ttk.Button(text="Browse...", command=browse_output)
 button3.grid(row=3, column=3)
 
-convert_button = Button(text= "Begin Conversion", font=convert_font, command=start_conversion)
+folder_header = ttk.Label(root, text="Conversion Settings", borderwidth=1, relief=SUNKEN, anchor=CENTER, justify="center")
+folder_header.grid(row=4, column=0, columnspan=2, pady=10, sticky="nsew")
+
+convert_button = ttk.Button(text= "Begin Conversion", command=start_conversion)
 convert_button.grid(row=7, column=0, columnspan=4, pady=50)
 
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
 root.grid_columnconfigure(3, weight=1)
+
+sv_ttk.set_theme("dark")
 
 root.mainloop()
